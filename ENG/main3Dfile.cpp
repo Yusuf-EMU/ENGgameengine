76deb40e6f1cdf73674984082eeb6237e17789ff
Yusuf-EMU <56773648+Yusuf-EMU@users.noreply.github.com>
2020-05-06 11:13:24 -0700

    Added Sphere Mapping + the bump mapping is broken now :(

--- ENG/main3Dfile.cpp
+++ ENG/main3Dfile.cpp
@@ -2,11 +2,13 @@
  g++ main.cpp -omain -framework OpenGL -framework GLUT -lfreeimage
 for debug
 Compile with
-    g++ main.cpp -omain -framework OpenGL -framework GLUT -lfreeimage -o2 
+    g++ main.cpp -omain -framework OpenGL -framework GLUT -lfreeimage -O2 
 for release
 */
-#include <GLUT/glut.h>  // GLUT, include glu.h and gl.h
-#include <GL/freeglut.h>
+#include <stdlib.h>  
+#include <OpenGL/gl.h>  
+#include <GLUT/glut.h>  
+#include <OpenGL/glext.h> 
 #include <math.h>
 #include <OpenGL/gl3.h>
 
@@ -17,8 +19,15 @@ for release
 #include <iostream>
 #include <string.h>
 #include "phyiscs.h"
+#include <utility>
 #include <thread>
+#include <chrono>
 #include <FreeImage.h>
+
+#if ( (defined(__MACH__)) && (defined(__APPLE__)) )  
+#else  
+glewInit();  
+#endif  
  
 float pyramidplace;
 
@@ -29,11 +38,15 @@ clock_t t;
 //Texture init
 //list of texture ids
 int texID[2];
-
-
+//bumpMaps
+int bumpID[2];
+//GL Material parms
+GLfloat ambientGenericMaterial[] = {0.2f, 0.2f, 0.2f, 1.0f};
+GLfloat specularGenericMaterial[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat shininessGenericMaterial =  50.0;
 //Aniso init
-GLuint g_samplers;
-GLfloat maxAniso = 4.0f;
+GLuint g_samplers[4];
+GLfloat maxAniso = 2.0f;
 //Creating a cube with Physics
 BoxPhysicsBody3d Cube;
 //Creating a list of cubes for use with collision
@@ -65,6 +78,12 @@ char* textureFileNames[2] = {
    "3dpapaya.jpg",
    "8514731789_86e9777367_z.jpg"
 };
+
+//normal maps for reference
+char* normalFileNames[1] = {
+   "bump_bark.jpeg"
+};
+
 
 static unsigned int CompileShader(unsigned int type, const std::string& source) {
    unsigned int id = glCreateShader(type);
@@ -134,13 +153,11 @@ void initGL() {
    GLfloat shininessLight[] = { 50.0 };
    GLfloat lightColor[] = {0.6f, 0.6f, 0.6f, 1.0f};
    GLfloat lightPos[] ={10,0,-15,1};
-
    glEnable( GL_MULTISAMPLE );
    glEnable(GL_LIGHTING);
    glEnable(GL_LIGHT0);
    glShadeModel (GL_SMOOTH);
    glEnable(GL_COLOR_MATERIAL);
-
    glLightfv(GL_LIGHT0, GL_AMBIENT, ambientLight);
    //Diffuse (non-shiny) light component
    glLightfv(GL_LIGHT0, GL_DIFFUSE, lightColor);
@@ -151,6 +168,14 @@ void initGL() {
    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
    glClearDepth(1.0f);                   // Set background depth to farthest
    //glTexEnvfv(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, decal);
+
+   //Sphere Mapping init
+   glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, GL_SPHERE_MAP);
+   glTexGeni(GL_T, GL_TEXTURE_GEN_MODE, GL_SPHERE_MAP);
+   glEnable(GL_TEXTURE_GEN_S);
+   glEnable(GL_TEXTURE_GEN_T);
+
+
    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
@@ -178,29 +203,42 @@ void initGL() {
 /* Handler for window-repaint event. Called back when the window first appears and
    whenever the window needs to be re-painted. */
 void display() {
+   GLuint linearFiltering = 0;
+   unsigned int layer = glCreateProgram();
+   int baseImageLoc = glGetUniformLocation(layer, "Base");
+   int normalMapLoc = glGetUniformLocation(layer, "NormalMap");
+
+   glUseProgram(layer);
+   glUniform1i(baseImageLoc, 0); // Texture unit 0 is for base images.
+   glUniform1i(normalMapLoc, 1); // Texture unit 1 is for normal maps.
+   //glUniform1i(shadowMapLoc, 4); // Texture unit 4 is for shadow maps.
+
    t = clock();
    glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);  // Clear color and depth buffers
    glMatrixMode(GL_PROJECTION);     // To operate on model-view matrix
    glMatrixMode(GL_MODELVIEW);
-   // Render a color-cube consisting of 6 quads with different colors
    glLoadIdentity();                 // Reset the model-view matrix
    glEnable(GL_TEXTURE_2D); 
    //std::thread threadt(engTranslatef, 1.5f, 0, -6.0f, Cube);
    engTranslatef(1.5f, 0, -6.0f, Cube);
-   // glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, shine);
    glEnable(GL_TEXTURE_GEN_S); //enable texture coordinate generation
     glEnable(GL_TEXTURE_GEN_T);
-    glColorMaterial ( GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE ) ;
     glEnable ( GL_COLOR_MATERIAL ) ;
+   glEnable(GL_TEXTURE_2D);  
+    glActiveTexture(GL_TEXTURE0 + 0);  
+    glBindTexture(GL_TEXTURE_2D, bumpID[0]);
+    glBindSampler(0, linearFiltering); 
+   glActiveTexture(GL_TEXTURE0 + 1);  
     glBindTexture(GL_TEXTURE_2D, texID[0]);
+    glBindSampler(2, linearFiltering);
+    glDisable(GL_TEXTURE_2D);   
     //threadt.join();
     glutSolidCube(2);
     glDisable(GL_TEXTURE_GEN_S); //enable texture coordinate generation
     glDisable(GL_TEXTURE_GEN_T);
-    glBindTexture(GL_TEXTURE_2D, texID[-1]);
-
  
    // Render a pyramid consists of 4 triangles
+   glMaterialfv( GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE,  ambientGenericMaterial) ;
    glLoadIdentity();
    glTranslatef(-1.5f, sin(pyramidplace), -6.0f);  // Move left and into the screen
  
@@ -246,6 +284,7 @@ void display() {
 }
 
 void loadTextures() {
+    glActiveTexture(GL_TEXTURE0);  
     int i;
     glGenTextures( 2, (GLuint *) texID );  // Get the texture object IDs.
     for (i = 0; i < 2; i++) {
@@ -278,10 +317,56 @@ void loadTextures() {
             glBindTexture( GL_TEXTURE_2D, texID[i] );  // Will load image data into texture object #i
             glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, imgWidth, imgHeight, 0, format,
                                 GL_UNSIGNED_BYTE, imgData);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); // Required since there are no mipmaps.
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_REPEAT); // Required since there are no mipmaps.
         }
         else {
             printf("Failed to get texture data from %s\n", textureFileNames[i]);
+        }
+    }
+}
+
+void programe (){
+
+}
+
+void loadNormals() {
+    glActiveTexture(GL_TEXTURE1);  
+    int i;
+    glGenTextures( 2, (GLuint *) bumpID );  // Get the texture object IDs.
+    for (i = 0; i < 2; i++) {
+        void* imgData;  // Pointer to image color data read from the file.
+        int imgWidth;   // The width of the image that was read.
+        int imgHeight;  // The height.
+        FREE_IMAGE_FORMAT format = FreeImage_GetFIFFromFilename(normalFileNames[i]);
+        if (format == FIF_UNKNOWN) {
+            printf("Unknown file type for texture image file %s\n", normalFileNames[i]);
+            continue;
+        }
+        FIBITMAP* bitmap = FreeImage_Load(format, normalFileNames[i], 0);  // Read image from file.
+        if (!bitmap) {
+            printf("Failed to load image %s\n", normalFileNames[i]);
+            continue;
+        }
+        FIBITMAP* bitmap2 = FreeImage_ConvertTo24Bits(bitmap);  // Convert to RGB or BGR format
+        FreeImage_Unload(bitmap);
+        imgData = FreeImage_GetBits(bitmap2);     // Grab the data we need from the bitmap.
+        imgWidth = FreeImage_GetWidth(bitmap2);
+        imgHeight = FreeImage_GetHeight(bitmap2);
+        if (imgData) {
+            printf("bump image loaded from file %s, size %dx%d\n", 
+                                     normalFileNames[i], imgWidth, imgHeight);
+            int format; // The format of the color data in memory, depends on platform.
+            if ( FI_RGBA_RED == 0 )
+               format = GL_RGB;
+            else
+               format = GL_BGR;
+            glBindTexture( GL_TEXTURE_2D, texID[i] );  // Will load image data into texture object #i
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, imgWidth, imgHeight, 0, format,
+                                GL_UNSIGNED_BYTE, imgData);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_REPEAT); // Required since there are no mipmaps.
+        }
+        else {
+            printf("Failed to get bump data from %s\n", textureFileNames[i]);
         }
     }
 }
@@ -313,6 +398,7 @@ int main(int argc, char** argv) {
    glutDisplayFunc(display);       // Register callback handler for window re-paint event
    glutReshapeFunc(reshape);       // Register callback handler for window re-size event
    loadTextures();
+   loadNormals();
    initGL();                       // Our own OpenGL initialization
    glutKeyboardFunc(processNormalKeys);
    glEnableVertexAttribArray(0);
